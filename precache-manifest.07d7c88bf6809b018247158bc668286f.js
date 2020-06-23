bc9c64c5d859e52a1a017662ae494725e161f8c9
Sh-Egor <38857171+Sh-Egor@users.noreply.github.com>
2020-06-23 12:09:01 +0400

    Updates

---
+++ precache-manifest.07d7c88bf6809b018247158bc668286f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3236ebd2eb868e0c5cc00598da14219",
+    "url": "/rest_on_the_don/index.html"
+  },
+  {
+    "revision": "6c58ce690d3e3c8dab4d",
+    "url": "/rest_on_the_don/static/css/main.3162c4e0.chunk.css"
+  },
+  {
+    "revision": "0f0e0d02253f1708e07b",
+    "url": "/rest_on_the_don/static/js/2.6e72eec8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rest_on_the_don/static/js/2.6e72eec8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c58ce690d3e3c8dab4d",
+    "url": "/rest_on_the_don/static/js/main.e5b2ba39.chunk.js"
+  },
+  {
+    "revision": "807da2d19f968300ac94",
+    "url": "/rest_on_the_don/static/js/runtime-main.4111f185.js"
+  }
+]);
